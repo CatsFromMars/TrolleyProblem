851be21e9ae2affabf3d7c9957ec1fb7499c972d
CatsFromMars <mduque@andrew.cmu.edu>
2015-11-10 19:22:55 -0500

    initial commit

---
+++ Assets/Scripts/PlatformBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformBehavior : MonoBehaviour {
+
+	public bool activated = true;
+	public float speed = 2f;
+	
+	// Update is called once per frame
+	void Update () {
+		if (activated) {
+			transform.position += speed*Vector3.down*Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Scripts/ScenarioController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScenarioController : MonoBehaviour {
+
+	public bool controllerFlipped = false;
+	public PlatformBehavior singlePlatform;
+	public PlatformBehavior groupPlatform;
+
+	// Update is called once per frame
+	void Update () {
+		if (controllerFlipped) {
+			singlePlatform.activated = true;
+			groupPlatform.activated = false;
+		}
+		else {
+			singlePlatform.activated = false;
+			groupPlatform.activated = true;
+		}
+	}
+}

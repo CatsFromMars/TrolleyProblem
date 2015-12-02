351fc7010ba08869cc053a0e935d9ee388d26fd7
Ivan Wang <icw@cmu.edu>
2015-12-02 17:38:43 -0500

    new voice acting with 3d audio

--- Assets/Scripts/ScenarioController.cs
+++ Assets/Scripts/ScenarioController.cs
@@ -12,6 +12,8 @@ public class ScenarioController : MonoBehaviour {
 	public GameObject areaLight;
 	public GameObject electricity;
 
+	public AudioSource RSSource;
+	public AudioSource AISource;
 	public AudioClip[] voiceClips;
 	public AudioClip[] soundEffects;
 
@@ -27,10 +29,8 @@ public class ScenarioController : MonoBehaviour {
 	private float decisionTime; // time decision is introduced
 	private float actionTime; // time when player has acted
 
-	private AudioSource source;
 
 	void Start() {
-		source = GetComponent<AudioSource>();
 		// TODO: fade in
 		Debug.Log("Scene loaded. Press any key to begin.");
 	}
@@ -65,7 +65,7 @@ public class ScenarioController : MonoBehaviour {
 				// Intro phase
 				case 0:
 					Debug.Log("Hello, and welcome to the facility. Today, you will be assisting a few construction workers as they revamp the lab.");
-					yield return StartCoroutine(PlaySoundAndWait(0, 7f)); // RS-01
+					yield return StartCoroutine(PlaySoundAndWait(0, RSSource, 7f)); // RS-01
 
 					controller.Activated = true; // control panel lights up
 					controller.buttonState = ButtonState.NotPressed;
@@ -77,7 +77,7 @@ public class ScenarioController : MonoBehaviour {
 					pLight2.SetActive(true);
 
 					Debug.Log("The control panel in front of you controls the workers' platforms. Please use the buttons to lower the platforms to the pipe. Note that only one platform can move at a time.");
-					yield return StartCoroutine(PlaySoundAndWait(1, 0f)); // RS-02
+					yield return StartCoroutine(PlaySoundAndWait(1, RSSource, 0f)); // RS-02
 					yield return StartCoroutine(WaitForElevators()); // wait for elevators to reach right level
 					break;
 
@@ -88,7 +88,7 @@ public class ScenarioController : MonoBehaviour {
 					yield return new WaitForSeconds(1f);
 					controller.buttonState = ButtonState.NotPressed;
 					Debug.Log("Good work. Now that the platforms are in the correct place--");
-					yield return StartCoroutine(PlaySoundAndWait(2, 2.8f)); // RS-03
+					yield return StartCoroutine(PlaySoundAndWait(2, RSSource, 2.8f)); // RS-03
 					break;
 
 				// Power outage
@@ -97,7 +97,7 @@ public class ScenarioController : MonoBehaviour {
 					pLight1.SetActive(false);
 					pLight2.SetActive(false);
 					playerLighting.SetActive(false);
-					yield return StartCoroutine(PlaySoundAndWait(soundEffects[0], 2f)); // lights-off
+					yield return StartCoroutine(PlaySoundAndWait(soundEffects[0], AISource, 2f)); // lights-off
 
 					if (Config.Group != RGroup.LeverControl) {
 						// TODO: whirring sound effect for disappearing control panel.
@@ -108,19 +108,19 @@ public class ScenarioController : MonoBehaviour {
 					yield return new WaitForSeconds(1f);
 
 					Debug.Log("Analyzing system failure. Please hold.");
-					yield return StartCoroutine(PlaySoundAndWait(3, 4f)); // AI-01
+					yield return StartCoroutine(PlaySoundAndWait(3, AISource, 4f)); // AI-01
 
 					Debug.Log("Looks like we are experiencing some technical difficulties. Please stay calm as we investigate.");
-					yield return StartCoroutine(PlaySoundAndWait(4, 6.5f)); // RS-04
+					yield return StartCoroutine(PlaySoundAndWait(4, RSSource, 6.5f)); // RS-04
 					break;
 
 				// Lights restored
 				case 3:
 					// Lights on subject's platform appear, with SE
 					playerLighting.SetActive(true);
-					yield return StartCoroutine(PlaySoundAndWait(soundEffects[1], 1f)); // lights-on
+					yield return StartCoroutine(PlaySoundAndWait(soundEffects[1], AISource, 1f)); // lights-on
 					Debug.Log("Lights in section C are now online. Restoring power...");
-					yield return StartCoroutine(PlaySoundAndWait(5, 6f)); // AI-02
+					yield return StartCoroutine(PlaySoundAndWait(5, AISource, 6f)); // AI-02
 
 					// Lights on platforms return
 					areaLight.SetActive(false);
@@ -130,7 +130,7 @@ public class ScenarioController : MonoBehaviour {
 
 					if (Config.Group != RGroup.LeverControl) {
 						Debug.Log("Please sit tight-- we're sending a guy to fix the control panel.");
-						yield return StartCoroutine(PlaySoundAndWait(6, 4f)); // RS-05
+						yield return StartCoroutine(PlaySoundAndWait(6, RSSource, 4f)); // RS-05
 
 						// fat man walks over to edge and bends down, looking at floor
 						fatMan.SetActive(true);
@@ -139,8 +139,8 @@ public class ScenarioController : MonoBehaviour {
 					}
 
 					Debug.Log("All power restored. Rebooting generator...");
-					yield return StartCoroutine(PlaySoundAndWait(7, 5f)); // AI-03
-					yield return StartCoroutine(PlaySoundAndWait(soundEffects[2], 5f)); // generator-charging
+					yield return StartCoroutine(PlaySoundAndWait(7, AISource, 5f)); // AI-03
+					yield return StartCoroutine(PlaySoundAndWait(soundEffects[2], AISource, 5f)); // generator-charging
 					break;
 
 				// Electricity! zap
@@ -148,20 +148,20 @@ public class ScenarioController : MonoBehaviour {
 					electricity.SetActive(true);
 					yield return new WaitForSeconds(2f);
 					Debug.Log("Warning: the electric generator has malfunctioned. Please keep away from the ground floor.");
-					yield return StartCoroutine(PlaySoundAndWait(8, 7f)); // AI-04
+					yield return StartCoroutine(PlaySoundAndWait(8, AISource, 7f)); // AI-04
 
 					// 5-person elevator begin to shake
 					groupPlatform.movingAnimation = true;
 					// TODO: guys on elevator appear alarmed
 					yield return new WaitForSeconds(1f);
 					Debug.Log("Danger! Platform A unstable. Brake failure in 20 seconds.");
-					yield return StartCoroutine(PlaySoundAndWait(9, 6f)); // AI-05
+					yield return StartCoroutine(PlaySoundAndWait(9, AISource, 6f)); // AI-05
 					break;
 
 				// Introduce the decision
 				case 5:
 					Debug.Log("Oh man! The elevator is going to fall! We don't have much time before it hits the floor and electrocutes everyone on it!");
-					yield return StartCoroutine(PlaySoundAndWait(10, 6f)); // RS-06
+					yield return StartCoroutine(PlaySoundAndWait(10, RSSource, 6f)); // RS-06
 
 					if (Config.Group == RGroup.LeverControl) {
 						controller.Activated = true;
@@ -171,20 +171,20 @@ public class ScenarioController : MonoBehaviour {
 						panelControlsPlatforms = true;
 
 						Debug.Log("Using the control panel, you can switch the falling platforms. But no matter what, we can only save one of them...");
-						yield return StartCoroutine(PlaySoundAndWait(11, 5f)); // RS-07
+						yield return StartCoroutine(PlaySoundAndWait(11, RSSource, 5f)); // RS-07
 						decisionTime = Time.time;
 						Debug.Log("Oh man oh man oh man. What do we do?");
-						yield return StartCoroutine(PlaySoundAndWait(12, 3f)); // RS-08
+						yield return StartCoroutine(PlaySoundAndWait(12, RSSource, 3f)); // RS-08
 					} else {
-						Debug.Log("The emergency brakes won't kick in unless the generator shorts. But the only way to short the electricity... The only way to stop the elevator is to push the worker in front of you and short the generator.");
-						yield return StartCoroutine(PlaySoundAndWait(13, 10f)); // RS-09
+						Debug.Log("The emergency brakes won't kick in unless the generator shorts. But the only way to short the electricity... The only way to stop the elevator is to push the worker in front of you onto the floor.");
+						yield return StartCoroutine(PlaySoundAndWait(13, RSSource, 10f)); // RS-09
 
 						decisionTime = Time.time;
 						// Enable pushing fat man
-						animationTrigger.SetActive (true);
+						animationTrigger.SetActive(true);
 
 						Debug.Log("Oh man oh man oh man. What do we do?");
-						yield return StartCoroutine(PlaySoundAndWait(12, 3f)); // RS-08
+						yield return StartCoroutine(PlaySoundAndWait(12, RSSource, 3f)); // RS-08
 					}
 
 					yield return StartCoroutine(WaitForDeath());
@@ -196,7 +196,8 @@ public class ScenarioController : MonoBehaviour {
 						groupPlatform.Activated = false;
 					} else {
 						if (pushedFatMan) {
-							fatMan.GetComponent<AudioSource>().PlayOneShot(soundEffects[3]); // scream
+							actionTime = Time.time;
+							yield return StartCoroutine(PlaySoundAndWait(soundEffects[3], fatMan.GetComponent<AudioSource>(), 1f)); // scream
 						}
 						groupPlatform.Activated = false;
 					}
@@ -204,14 +205,14 @@ public class ScenarioController : MonoBehaviour {
 
 				// Someone has died. Ending.
 				case 6:
-					// TODO: play SE, animation of floor crackling
 					Debug.Log("Bio material detected. Disengaging power supply.");
-					yield return StartCoroutine(PlaySoundAndWait(14, 5f)); // AI-06
+					yield return StartCoroutine(PlaySoundAndWait(14, AISource, 5f)); // AI-06
+					electricity.SetActive(false);
 
-					// TODO: Fade out scene
-					Debug.Log("This concludes the research experiment. Please remove your headgear.");
+					// Fade out scene
 					screenFader.SetTrigger(Animator.StringToHash("Fade"));
-					yield return StartCoroutine(PlaySoundAndWait(15, 5f)); // RS-10
+					Debug.Log("This concludes the research experiment. Please remove your headgear.");
+					//yield return StartCoroutine(PlaySoundAndWait(15, RSSource, 5f)); // RS-10
 					break;
 
 				// Results
@@ -238,21 +239,21 @@ public class ScenarioController : MonoBehaviour {
 					Debug.Log("gg");
 					break;
 			}
-			Debug.Log("switching state: " + state.ToString());
 		}
 	}
 
-	private IEnumerator PlaySoundAndWait(int sound, float seconds) {
-		return PlaySoundAndWait(voiceClips[sound], seconds);
+	private IEnumerator PlaySoundAndWait(int sound, AudioSource source, float seconds) {
+		return PlaySoundAndWait(voiceClips[sound], source, seconds);
 	}
 
-	private IEnumerator PlaySoundAndWait(AudioClip sound, float seconds) {
+	private IEnumerator PlaySoundAndWait(AudioClip sound, AudioSource source, float seconds) {
 		source.PlayOneShot(sound);
 
 		if (seconds > 0) {
 			yield return new WaitForSeconds(seconds);
 		}
 	}
+
 	private IEnumerator WaitForElevators() {
 		while (!(singlePlatform.reachedStopZone && groupPlatform.reachedStopZone)) {
 			yield return null;

3ea08ecf3357860040ab780c136b4a9f63f06d51
Ivan Wang <icw@cmu.edu>
2015-12-03 21:28:19 -0500

    increment heart rate log file

--- Assets/Scripts/Arduino.cs
+++ Assets/Scripts/Arduino.cs
@@ -8,9 +8,16 @@ public class Arduino : MonoBehaviour {
 	SerialPort port;
 	StreamWriter writer;
 
+	public string logDirectory = "Log";
 	private bool writing = false;
 
 	void Start() {
+
+			int fileCount = Directory.GetFiles(logDirectory, "*.txt", SearchOption.TopDirectoryOnly).Length + 1;
+			writer = new StreamWriter(logDirectory + "/heartRate" + fileCount.ToString("D2") + ".txt");
+			writer.WriteLine("Heart Rate Log - " + DateTime.Now.ToString("HH:mm:ss.ffff"));
+			writer.WriteLine("Current time,time (ms),BPM,EDR");
+
 		// connect to port for Arduino
 		port = new SerialPort("COM3", 9600);
 		port.ReadTimeout = 10;
@@ -21,15 +28,15 @@ public class Arduino : MonoBehaviour {
         port.Handshake = Handshake.None;
 
 		try {
-			//port.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
 			port.Open();
 			Debug.Log("connected successfully to Arduino");
 
 			// set up writer log
 			writing = true;
-			writer = new StreamWriter("Log/test.txt");
+			/*int fileCount = Directory.GetFiles(logDirectory, "*.txt", SearchOption.TopDirectoryOnly).Length + 1;
+			writer = new StreamWriter(logDirectory + "/heartRate" + fileCount.ToString("D2") + ".txt");
 			writer.WriteLine("Heart Rate Log - " + DateTime.Now.ToString("HH:mm:ss.ffff"));
-			writer.WriteLine("Current time,time (ms),BPM,EDR");
+			writer.WriteLine("Current time,time (ms),BPM,EDR");*/
 		} catch (Exception e) {
 			Debug.Log(e.ToString());
 			Debug.Log("could not connect to Arduino port. aborting");
